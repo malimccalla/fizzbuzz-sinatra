90c24e6a73685b916e5e5c61f681497404bf0cfc
Mali Michael <malimccalla@gmail.com>
2016-06-28 11:20:44 +0100

    Fix bug so that expected output is 'fizz' not 'fizzbuzz' for numbers divisible by 3 but not 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,6 @@
 def fizzbuzz(number)
   if (number.is_a? Integer) && (number > 0)
-    return "'#{number}' fizzed is fizzbuzz!" if (number % 3 == 0) && (number % 5 != 0)
+    return "'#{number}' fizzed is fizz!" if (number % 3 == 0) && (number % 5 != 0)
     return "'#{number}' fizzed is buzz!" if (number % 5 == 0) && (number % 3 != 0)
     return "'#{number}' fizzed is fizzbuzz!" if (number % 5 == 0) && (number % 3 == 0)
     number
